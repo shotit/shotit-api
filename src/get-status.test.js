0f16ab24bb8922d5341bd19f74e8b503e8d42f2b
Leslie Wong <79917148leslie@gmail.com>
2023-02-08 05:32:26 +0800

    Update get-status.js to fit Milvus

--- src/get-status.test.js
+++ src/get-status.test.js
@@ -28,22 +28,28 @@ test("GET /status", async () => {
   expect(response.statusCode).toBe(200);
   expect(response.headers["content-type"]).toMatch(/^application\/json/);
   expect(typeof response.body).toBe("object");
-  let numDocs = 0;
-  let totalSize = 0;
-  let lastModified = new Date(0);
-  for (const [_, server] of Object.entries(response.body)) {
-    for (const core of server) {
-      numDocs += core.index.numDocs;
-      totalSize += core.index.sizeInBytes;
-      lastModified =
-        lastModified > new Date(core.index.lastModified)
-          ? lastModified
-          : new Date(core.index.lastModified);
-    }
-  }
-  expect(numDocs).toBeGreaterThanOrEqual(0);
-  expect(numDocs).toBeGreaterThanOrEqual(0);
-  expect(lastModified).toBeInstanceOf(Date);
+  // let numDocs = 0;
+  // let totalSize = 0;
+  // let lastModified = new Date(0);
+  // for (const [_, server] of Object.entries(response.body)) {
+  //   for (const core of server) {
+  //     numDocs += core.index.numDocs;
+  //     totalSize += core.index.sizeInBytes;
+  //     lastModified =
+  //       lastModified > new Date(core.index.lastModified)
+  //         ? lastModified
+  //         : new Date(core.index.lastModified);
+  //   }
+  // }
+  // expect(numDocs).toBeGreaterThanOrEqual(0);
+  // expect(totalSize).toBeGreaterThanOrEqual(0);
+  // expect(lastModified).toBeInstanceOf(Date);
+
+  /* Use Milvus Statistics */
+  const errorCode = response.body?.status.error_code;
+  const rowCount = response.body?.data.row_count;
+  expect(errorCode).toBe("Success");
+  expect(rowCount).toBeGreaterThanOrEqual(0);
 });
 
 test("GET /status?id=a", async () => {
